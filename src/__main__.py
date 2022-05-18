d35ceb4201f8d79292c968947b5231bc15eeeffe
tassaron <rutachigal@gmail.com>
2022-05-17 20:54:11 -0400

    runnable without installation of package

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,11 @@
 """
 Entrypoint for `python -m nagcat` or the nagcat command
 """
-from nagcat.nagcat import main
+try:
+    from .nagcat import main
+except ImportError:
+    from nagcat import main
+
 
 if __name__ == "__main__":
     exit(main())
