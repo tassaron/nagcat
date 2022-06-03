723c4d7fafb501f183bc548ab27d55ab6afbba79
tassaron <rutachigal@gmail.com>
2022-06-03 08:42:45 -0400

    add more tests of day-transitions, better test names + docstrings

--- tests/test_reminders.py
+++ tests/test_reminders.py
@@ -30,7 +30,16 @@ def test_use_litterbox(config_and_litterbox):
     assert len(glob.glob(os.path.join(litterbox_dir, "*_0"))) == 1
 
 
-def test_reminders_pending_midday(config_and_litterbox):
+def test_reminder_not_pending_same_day(config_and_litterbox):
+    """Reminder for 14:00 is not pending before 14:00 on same day"""
+    _, reminders, litterbox_dir = config_and_litterbox
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 15)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert not nagcat.reminders_pending(reminders, litterbox_dir)
+
+
+def test_reminder_pending_same_day(config_and_litterbox):
+    """Reminder for 14:00 is pending after 14:00 on same day"""
     _, reminders, litterbox_dir = config_and_litterbox
     nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 15)
     nagcat.use_litterbox(reminders, litterbox_dir)
@@ -40,11 +49,45 @@ def test_reminders_pending_midday(config_and_litterbox):
     assert nagcat.reminders_pending(reminders, litterbox_dir)
 
 
-def test_reminders_pending_overnight(config_and_litterbox):
+def test_reminder_pending_tomorrow(config_and_litterbox):
+    """Reminder for 14:00 is pending after 14:00 after 1 day"""
     _, reminders, litterbox_dir = config_and_litterbox
     nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 15, 23)
     nagcat.use_litterbox(reminders, litterbox_dir)
     assert nagcat.reminders_pending(reminders, litterbox_dir)
-    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 16, 0)
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 16, 14)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert nagcat.reminders_pending(reminders, litterbox_dir)
+
+
+def test_reminder_not_pending_tomorrow(config_and_litterbox):
+    """Reminder for 14:00 is not pending before 14:00 after 1 day"""
+    _, reminders, litterbox_dir = config_and_litterbox
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 15, 23)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert nagcat.reminders_pending(reminders, litterbox_dir)
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 16)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert not nagcat.reminders_pending(reminders, litterbox_dir)
+
+
+def test_reminder_pending_after_two_days(config_and_litterbox):
+    """Reminder for 14:00 is pending after 14:00 after 2 days"""
+    _, reminders, litterbox_dir = config_and_litterbox
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 15, 23)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert nagcat.reminders_pending(reminders, litterbox_dir)
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 17, 14)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert nagcat.reminders_pending(reminders, litterbox_dir)
+
+
+def test_reminder_not_pending_after_two_days(config_and_litterbox):
+    """Reminder for 14:00 is not pending before 14:00 after 2 days"""
+    _, reminders, litterbox_dir = config_and_litterbox
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 15, 23)
+    nagcat.use_litterbox(reminders, litterbox_dir)
+    assert nagcat.reminders_pending(reminders, litterbox_dir)
+    nagcat.get_datetime_now = lambda: datetime.datetime(2022, 5, 17, 0)
     nagcat.use_litterbox(reminders, litterbox_dir)
     assert not nagcat.reminders_pending(reminders, litterbox_dir)
